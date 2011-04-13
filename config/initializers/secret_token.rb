56e73fa2db135e4c4d7f674aa56f9404c2b165fd
Gershon Bialer <gershon.bialer@gmail.com>
2011-04-13 18:45:13 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Strangerdash3::Application.config.secret_token = '3548b8f80f21cf8daeddfe98c1c55501789cef601ce5b2b01f505e33dee142e88d2e9f9f460fba480b9b8740f5d6cdea667e155e68a98714bec610c603c573ba'
