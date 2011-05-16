75f2a06f175de13170b24657e9709b035e30e572
Gershon Bialer <gershon.bialer@gmail.com>
2011-05-16 13:53:17 -0500

    Added js and route

--- Gemfile
+++ Gemfile
@@ -2,11 +2,12 @@ source 'http://rubygems.org'
 
 gem 'rails', '3.0.5'
 gem 'raydash'
+gem 'jquery-rails'
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
 gem 'sqlite3'
-
+gem 'raydash'
 # Use unicorn as the web server
 # gem 'unicorn'
 
--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,8 @@ Strangerdash3::Application.routes.draw do
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
-
+  get '/match' => 'match#index'
+  get '/match/next' => 'match#next'
   # Sample resource route with options:
   #   resources :products do
   #     member do
@@ -48,7 +49,7 @@ Strangerdash3::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "match#index"
 
   # See how all your routes lay out with "rake routes"
 
