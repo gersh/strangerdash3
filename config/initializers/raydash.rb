dd9cff6705fbfa5db534cc504634ac318d20fc96
Gershon Bialer <gershon.bialer@gmail.com>
2011-11-29 00:15:08 -0600

    Fixed stuff for release

--- config/initializers/raydash.rb
+++ config/initializers/raydash.rb
@@ -1,7 +1,4 @@
 Raydash.setup do |config|
-  config.userid = "2"
-  config.secret = "MzhhZGQ4MmEtNzk5NS00OTM3LTliNTEtN2I5YzVlNTM1MDNm"
-#  config.secret = "MTE3MjZkMjAtOGJlNy00NjI0LThlYzUtZGMzMzJiMmRkMjdi"
-#  config.userid = "t1"
-#  config.secret = "MzVjMjVmMjctZWI0My00ODc5LTgyZmQtMGZhYTYxYzZjOTAw"
+  config.userid = ""
+  config.secret = ""
 end
